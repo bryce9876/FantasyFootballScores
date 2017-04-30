cbb1618eca6d587ed6dce9b998d97ed5bb5b0d2d
Bryce <mathesonb@student.unimelb.edu.au>
2017-04-30 21:58:46 +1000

    updated for round 7 (early ay)

--- myScript.js
+++ myScript.js
@@ -20,7 +20,7 @@ window.onclick = function(e) {
 function changeGame(gameNumber) {
 
 	// Upate round value below depending on round
-	var round = 6
+	var round = 7;
 	var game1ID = 6001 + (round-1)*9;
 
 	var initialURL = "http://live.fanfooty.com.au/game/matchcentre.html?id=";
