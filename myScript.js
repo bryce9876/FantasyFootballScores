3d2b8a9970b30a5c24c782fb5190c7c5b86d525c
Bryce <mathesonb@student.unimelb.edu.au>
2017-05-26 02:35:34 +1000

    didnt account for the bye last round - think its actually fixed now

--- myScript.js
+++ myScript.js
@@ -25,11 +25,11 @@ function changeGame(gameNumber) {
 
 	// Upate round value below depending on round
 	var round = 10;
-  //var total_byes = 1;
-	var game1ID = 6000 + (round-1)*9;
+  var total_byes = 1;
+	var game1ID = 6000 + (round-1)*9 - total_byes;
 
   // When fanfooty starts working with https, change below to https too
-	var initialURL = "https://live.fanfooty.com.au/game/matchcentre.html?id=";
+	var initialURL = "http://live.fanfooty.com.au/game/matchcentre.html?id=";
 	var gameXID = game1ID + gameNumber -1;
 
     document.getElementById('iframe').src = initialURL + gameXID;
